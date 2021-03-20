078205a2a01a6ff3d928c09c137db5fcf499698d
gitdxj <frdxj98@gmail.com>
2021-03-20 11:35:55 +0100

    add neural networks model and ModifiedTensorBoard

--- agent_code/savage_agent/utils.py
+++ agent_code/savage_agent/utils.py
@@ -1,3 +1,9 @@
+import tensorflow as tf
+import os
+
+ACTION_NUM = 6
+MODEL_NAME = "2*256"
+
 
 def get_state_matrix(state):
     """
@@ -32,5 +38,63 @@ def get_state_matrix(state):
             if explosion[i][j] > 0:  # explosion
                 field[i][j] = 7
     return field
+
+
+class ModifiedTensorBoard(tf.keras.callbacks.TensorBoard):
+
+    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.step = 1
+        self.writer = tf.summary.create_file_writer(self.log_dir)
+        self._log_write_dir = os.path.join(self.log_dir, MODEL_NAME)
+
+    # Overriding this method to stop creating default log writer
+    def set_model(self, model):
+        pass
+
+    # Overrided, saves logs with our step number
+    # (otherwise every .fit() will start writing from 0th step)
+    def on_epoch_end(self, epoch, logs=None):
+        self.update_stats(**logs)
+
+    # Overrided
+    # We train for one batch only, no need to save anything at epoch end
+    def on_batch_end(self, batch, logs=None):
+        pass
+
+    # Overrided, so won't close writer
+    def on_train_end(self, _):
+        pass
+
+    def on_train_batch_end(self, batch, logs=None):
+        pass
+
+    # Custom method for saving own metrics
+    # Creates writer, writes custom metrics and closes writer
+    def update_stats(self, **stats):
+        self._write_logs(stats, self.step)
+
+    def _write_logs(self, logs, index):
+        with self.writer.as_default():
+            for name, value in logs.items():
+                tf.summary.scalar(name, value, step=index)
+                self.step += 1
+                self.writer.flush()
+
+
+def create_model():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.SimpleRNN(120, return_sequences=True),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.SimpleRNN(120, return_sequences=False),
+        tf.keras.layers.Dropout(0.2),
+
+        tf.keras.layers.Dense(64),
+        tf.keras.layers.Dense(ACTION_NUM, activation='linear')
+    ])
+    model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=0.001), metrics=['accuracy'])
+    return model
 
 
